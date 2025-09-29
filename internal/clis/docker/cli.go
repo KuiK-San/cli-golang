ea6ccc0902cbe627d2c89178922925a3ea4351b5
Guilherme Casagrande <thegui4000@gmail.com>
2025-09-29 10:20:17 -0300

    --wip-- [skip ci]

--- internal/clis/docker/cli.go
+++ internal/clis/docker/cli.go
@@ -1,7 +1,50 @@
 package docker
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Cli(params []string) {
-	fmt.Println("enviado os parametros para a DockerCLI", params)
+	if len(params[1:]) < 1 {
+		fmt.Println("Nenhum comando encontrado")
+		helpDefault()
+		return
+	}
+
+	switch params[1] {
+	case "restart":
+		restartContainer(params[2])
+	case "restart-push":
+		restartContainer(params[2])
+	case "--help", "-h":
+		helpDefault()
+	default:
+		fmt.Print(params[1:], "Não é um comando valido")
+		helpDefault()
+	}
+}
+
+func helpDefault() {
+	fmt.Println("")
+	fmt.Println(`CLI-Server - Ferramenta de linha de comando multifuncional
+
+	USO:
+	cli-server docker [OPÇÕES]
+
+	COMANDOS PRINCIPAIS:
+	restart   		Reinicia e rebuilda o container
+	restart-push	Executa o push do repositório, reinicia e rebuilda o container
+	
+	OPÇÕES GLOBAIS:
+	-h, --help     Mostra esta mensagem de ajuda`,
+	)
+	fmt.Println("")
+}
+
+func restartContainer(container string) {
+	// TODO: fazer o restart do container
+}
+
+func pushRepo() {
+	// TODO: fazer o pull do repositorio
 }
--- internal/clis/git/cli.go
+++ internal/clis/git/cli.go
@@ -1,7 +1,40 @@
 package git
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func Cli(params []string) {
-	fmt.Println("Recebido os Parametros na funcionalidade GIT", params)
+	if len(os.Args[1:]) < 1 {
+		fmt.Println("Nenhum comando encontrado")
+		helpDefault()
+		return
+	}
+
+	switch os.Args[1] {
+	case "configure":
+		// TODO: escreve eval "$(ssh-agent -s)" e ssh-add ~/.ssh/github no terminal atual do usuario
+	case "--help", "-h":
+		helpDefault()
+	default:
+		fmt.Print(os.Args[1:], "Não é um comando valido")
+		helpDefault()
+	}
+}
+
+func helpDefault() {
+	fmt.Println("")
+	fmt.Println(`CLI-Server - Ferramenta de linha de comando multifuncional
+
+	USO:
+	cli-server git [SUBCOMANDOS] [OPÇÕES]
+
+	COMANDOS PRINCIPAIS:
+	configure   Configura o git na pasta atual
+	
+	OPÇÕES GLOBAIS:
+	-h, --help     Mostra esta mensagem de ajuda`,
+	)
+	fmt.Println("")
 }
